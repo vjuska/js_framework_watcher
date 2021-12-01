c5bc0ed7cdeb4135c525e925d31f09d81ed14f98
Vic <vjuska24@gmail.com>
2021-11-30 18:34:13 -0600

    Added 2nd method for Angularjs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
 
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "https://api.github.com" : "/";
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <button @click="indexGitapi()">Click me</button>
+    {{ vuejs.stargazers_count }}
+    {{ vuejs.watchers }}
+    {{ vuejs.watchers_count }}
+    <button @click="indexAngularjs()">Click me</button>
+    {{ angularjs.stargazers_count }}
+    {{ angularjs.watchers }}
+    {{ angularjs.watchers_count }}
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      vuejs: {},
+      angularjs: {},
+    };
+  },
+  created: function () {},
+  methods: {
+    indexGitapi: function () {
+      axios.get("/repos/vuejs/vue").then((response) => {
+        console.log("github", response.data);
+        this.vuejs = response.data;
+      });
+    },
+    indexAngularjs: function () {
+      axios.get("/repos/angular/angular.js").then((response) => {
+        console.log("github", response.data);
+        this.angularjs = response.data;
+      });
+    },
   },
 };
 </script>
